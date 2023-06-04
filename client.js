03f01204196d96d178e5135e3c23057b611f1fde
bronifty <bronifty@gmail.com>
2023-06-04 22:45:42 +0000

    this

--- client.js
+++ client.js
@@ -1,28 +1,35 @@
 let currentPathname = window.location.pathname;
 
+// instead of replacing the body directly, we are going to use a new navigation function that adds jsx search param for the server, which will control flow to a branch that renders JSX to client JSX, meaning, it makes all the server calls first, gets the data in JSX object format and sends it to the client as JSX instead of HTML string
 async function navigate(pathname) {
   currentPathname = pathname;
-
-  // Fetch HTML for the route we're navigating to.
-  const response = await fetch(pathname);
-  const html = await response.text();
-  console.log("html: ", html);
-
+  const response = await fetch(pathname + "?jsx");
+  const jsonString = await response.text();
   if (pathname === currentPathname) {
-    // Get the part of HTML inside the <body> tag.
-    const bodyStartIndex = html.indexOf("<body>") + "<body>".length;
-    const bodyEndIndex = html.lastIndexOf("</body>");
-    const bodyHTML = html.slice(bodyStartIndex, bodyEndIndex);
-
-    // Replace the content on the page.
-    document.body.innerHTML = bodyHTML;
+    alert(jsonString);
   }
 }
+
+// async function navigate(pathname) {
+//   currentPathname = pathname;
+//   // Fetch HTML for the route we're navigating to.
+//   const response = await fetch(pathname);
+//   const html = await response.text();
+
+//   if (pathname === currentPathname) {
+//     // Get the part of HTML inside the <body> tag.
+//     const bodyStartIndex = html.indexOf("<body>") + "<body>".length;
+//     const bodyEndIndex = html.lastIndexOf("</body>");
+//     const bodyHTML = html.slice(bodyStartIndex, bodyEndIndex);
+
+//     // Replace the content on the page.
+//     document.body.innerHTML = bodyHTML;
+//   }
+// }
 
 window.addEventListener(
   "click",
   (e) => {
-    console.log("am i getting called? e.target: ", e.target);
     // Only listen to link clicks.
     if (e.target.tagName !== "A") {
       return;
--- server.js
+++ server.js
@@ -6,14 +6,19 @@ import sanitizeFilename from "sanitize-filename";
 createServer(async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   console.log(url);
+  // if first load, client is requested by index page
   if (url.pathname === "/client.js") {
     sendScript(res, "./client.js");
     return;
   }
 
   try {
-    // send HTML to response for page Router determines by req.url
-    await sendHTML(res, <Router url={url} />);
+    if (url.searchParams.has("jsx")) {
+      url.searchParams.delete("jsx");
+      await sendJSX(res, <Router url={url} />);
+    } else {
+      await sendHTML(res, <Router url={url} />);
+    }
   } catch (err) {
     console.error(err);
     res.writeHead(err.statusCode ?? 500);
@@ -104,6 +109,57 @@ function Footer({ author }) {
       {author} {new Date().getFullYear()}!
     </footer>
   );
+}
+
+async function sendJSX(res, jsx) {
+  const clientJSX = await renderJSXToClientJSX(jsx);
+  const clientJSXString = JSON.stringify(clientJSX, null, 2);
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(clientJSXString);
+}
+
+async function renderJSXToClientJSX(jsx) {
+  if (
+    typeof jsx === "string" ||
+    typeof jsx === "number" ||
+    typeof jsx === "boolean" ||
+    jsx == null
+  ) {
+    // Don't need to do anything special with these types.
+    return jsx;
+  } else if (Array.isArray(jsx)) {
+    // Process each item in an array.
+    return Promise.all(jsx.map((child) => renderJSXToClientJSX(child)));
+  } else if (jsx != null && typeof jsx === "object") {
+    if (jsx.$$typeof === Symbol.for("react.element")) {
+      if (typeof jsx.type === "string") {
+        // This is a component like <div />.
+        // Go over its props to make sure they can be turned into JSON.
+        return {
+          ...jsx,
+          props: await renderJSXToClientJSX(jsx.props),
+        };
+      } else if (typeof jsx.type === "function") {
+        // This is a custom React component (like <Footer />).
+        // Call its function, and repeat the procedure for the JSX it returns.
+        const Component = jsx.type;
+        const props = jsx.props;
+        const returnedJsx = await Component(props);
+        return renderJSXToClientJSX(returnedJsx);
+      } else throw new Error("Not implemented.");
+    } else {
+      // This is an arbitrary object (for example, props, or something inside of them).
+      // Go over every value inside, and process it too in case there's some JSX in it.
+      return Object.fromEntries(
+        await Promise.all(
+          Object.entries(jsx).map(async ([propName, value]) => [
+            propName,
+            await renderJSXToClientJSX(value),
+          ])
+        )
+      );
+    }
+  } else throw new Error("Not implemented");
 }
 
 async function sendHTML(res, jsx) {
