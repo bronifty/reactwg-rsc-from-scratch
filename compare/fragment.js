3b281b76bdb87aee2f032223dcbefcd1842c10bd
bronifty <bronifty@gmail.com>
2023-06-07 03:33:05 +0000

    support-for-fragments

---
+++ compare/fragment.js
@@ -0,0 +1,19 @@
+{
+  '$$typeof': Symbol(react.element),
+  type: Symbol(react.fragment),
+  key: null,
+  ref: null,
+  props: {
+    children: {
+      '$$typeof': Symbol(react.element),
+      type: 'footer',
+      key: null,
+      ref: null,
+      props: [Object],
+      _owner: null,
+      _store: {}
+    }
+  },
+  _owner: null,
+  _store: {}
+}
---
+++ compare/jsx.js
@@ -0,0 +1,341 @@
+renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'section',
+     key: null,
+     ref: null,
+     props: { children: [ [Object], [Object] ] },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX {
+     children: [
+       {
+         '$$typeof': Symbol(react.element),
+         type: 'h1',
+         key: null,
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       },
+       {
+         '$$typeof': Symbol(react.element),
+         type: 'div',
+         key: null,
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       }
+     ]
+   }
+   renderJSXToClientJSX [
+     {
+       '$$typeof': Symbol(react.element),
+       type: 'h1',
+       key: null,
+       ref: null,
+       props: { children: 'Welcome to my blog' },
+       _owner: null,
+       _store: {}
+     },
+     {
+       '$$typeof': Symbol(react.element),
+       type: 'div',
+       key: null,
+       ref: null,
+       props: { children: [Array] },
+       _owner: null,
+       _store: {}
+     }
+   ]
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'h1',
+     key: null,
+     ref: null,
+     props: { children: 'Welcome to my blog' },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX { children: 'Welcome to my blog' }
+   renderJSXToClientJSX Welcome to my blog
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'div',
+     key: null,
+     ref: null,
+     props: { children: [ [Object], [Object] ] },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX {
+     children: [
+       {
+         '$$typeof': Symbol(react.element),
+         type: [AsyncFunction: Post],
+         key: 'dog',
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       },
+       {
+         '$$typeof': Symbol(react.element),
+         type: [AsyncFunction: Post],
+         key: 'hello-world',
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       }
+     ]
+   }
+   renderJSXToClientJSX [
+     {
+       '$$typeof': Symbol(react.element),
+       type: [AsyncFunction: Post],
+       key: 'dog',
+       ref: null,
+       props: { slug: 'dog' },
+       _owner: null,
+       _store: {}
+     },
+     {
+       '$$typeof': Symbol(react.element),
+       type: [AsyncFunction: Post],
+       key: 'hello-world',
+       ref: null,
+       props: { slug: 'hello-world' },
+       _owner: null,
+       _store: {}
+     }
+   ]
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: [AsyncFunction: Post],
+     key: 'dog',
+     ref: null,
+     props: { slug: 'dog' },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: [AsyncFunction: Post],
+     key: 'hello-world',
+     ref: null,
+     props: { slug: 'hello-world' },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'section',
+     key: null,
+     ref: null,
+     props: { children: [ [Object], [Object] ] },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX {
+     children: [
+       {
+         '$$typeof': Symbol(react.element),
+         type: 'h2',
+         key: null,
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       },
+       {
+         '$$typeof': Symbol(react.element),
+         type: 'article',
+         key: null,
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       }
+     ]
+   }
+   renderJSXToClientJSX [
+     {
+       '$$typeof': Symbol(react.element),
+       type: 'h2',
+       key: null,
+       ref: null,
+       props: { children: [Object] },
+       _owner: null,
+       _store: {}
+     },
+     {
+       '$$typeof': Symbol(react.element),
+       type: 'article',
+       key: null,
+       ref: null,
+       props: { children: 'yes hello this is dog' },
+       _owner: null,
+       _store: {}
+     }
+   ]
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'h2',
+     key: null,
+     ref: null,
+     props: {
+       children: {
+         '$$typeof': Symbol(react.element),
+         type: 'a',
+         key: null,
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       }
+     },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX {
+     children: {
+       '$$typeof': Symbol(react.element),
+       type: 'a',
+       key: null,
+       ref: null,
+       props: { href: '/dog', children: 'dog' },
+       _owner: null,
+       _store: {}
+     }
+   }
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'a',
+     key: null,
+     ref: null,
+     props: { href: '/dog', children: 'dog' },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX { href: '/dog', children: 'dog' }
+   renderJSXToClientJSX /dog
+   renderJSXToClientJSX dog
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'article',
+     key: null,
+     ref: null,
+     props: { children: 'yes hello this is dog' },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX { children: 'yes hello this is dog' }
+   renderJSXToClientJSX yes hello this is dog
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'section',
+     key: null,
+     ref: null,
+     props: { children: [ [Object], [Object] ] },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX {
+     children: [
+       {
+         '$$typeof': Symbol(react.element),
+         type: 'h2',
+         key: null,
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       },
+       {
+         '$$typeof': Symbol(react.element),
+         type: 'article',
+         key: null,
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       }
+     ]
+   }
+   renderJSXToClientJSX [
+     {
+       '$$typeof': Symbol(react.element),
+       type: 'h2',
+       key: null,
+       ref: null,
+       props: { children: [Object] },
+       _owner: null,
+       _store: {}
+     },
+     {
+       '$$typeof': Symbol(react.element),
+       type: 'article',
+       key: null,
+       ref: null,
+       props: { children: 'hello\n' },
+       _owner: null,
+       _store: {}
+     }
+   ]
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'h2',
+     key: null,
+     ref: null,
+     props: {
+       children: {
+         '$$typeof': Symbol(react.element),
+         type: 'a',
+         key: null,
+         ref: null,
+         props: [Object],
+         _owner: null,
+         _store: {}
+       }
+     },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX {
+     children: {
+       '$$typeof': Symbol(react.element),
+       type: 'a',
+       key: null,
+       ref: null,
+       props: { href: '/hello-world', children: 'hello-world' },
+       _owner: null,
+       _store: {}
+     }
+   }
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'a',
+     key: null,
+     ref: null,
+     props: { href: '/hello-world', children: 'hello-world' },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX { href: '/hello-world', children: 'hello-world' }
+   renderJSXToClientJSX /hello-world
+   renderJSXToClientJSX hello-world
+   renderJSXToClientJSX {
+     '$$typeof': Symbol(react.element),
+     type: 'article',
+     key: null,
+     ref: null,
+     props: { children: 'hello\n' },
+     _owner: null,
+     _store: {}
+   }
+   renderJSXToClientJSX { children: 'hello\n' }
+   renderJSXToClientJSX hello
+   
+  
